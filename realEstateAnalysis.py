f987f1037cbb3c82e555649d7cb7eb99d4558828
nick-carroll1 <112578073+nick-carroll1@users.noreply.github.com>
2022-09-18 16:55:14 +0000

    Updated analysis to output a dataframe predicting prices

--- realEstateAnalysis.py
+++ realEstateAnalysis.py
@@ -1,85 +1,66 @@
 # Import the necessary packages
 import dask.dataframe as dd
 import dask
+from dask.distributed import Client
 from sklearn import linear_model
-import matplotlib.pyplot as plt
 
 # Read in the data
 def readData(file):
     datadf = dd.read_csv(file, parse_dates=['sold_date'], infer_datetime_format=True)
     return datadf
 
-# Use a linear regression function (expected input groupby)
+# Use a linear regression function to predict prices (expected input groupby)
 def groupbyRegr(data):
     groupRegr = linear_model.LinearRegression()
-    return groupRegr.fit(data[['bed', 'bath', 'acre_lot', 'house_size']], data['price'])
+    droppedData = data[['bed', 'bath', 'acre_lot', 'house_size','price']].dropna()
+    if droppedData.shape[0] > 0:
+        groupRegr.fit(droppedData[['bed', 'bath', 'acre_lot', 'house_size']], droppedData['price'])
+        droppedData['Predicted Price'] = np.round(groupRegr.predict(droppedData[['bed', 'bath', 'acre_lot', 'house_size']]))
+        data['Predicted Price'] = droppedData['Predicted Price']
+    return data
 
 # Use a groupby function for a linear regression for each zip code
 def zipGroupArray(data):
-    return data.groupby('zip_code').apply(groupbyRegr)
+    data['Predicted Price'] = 0.
+    return data.groupby('zip_code').apply(groupbyRegr, meta = data)
 
-# Predict values for the linear regression
-def prediction(data, groupedData, zipCode):
-    return groupedData[zipCode].predict(data[data['zip_code']==zipCode].loc[:,['bed', 'bath', 'acre_lot', 'house_size']])
-
-# Plot data predictions against actual data
+# Plot data predictions against actual data (unused needs to be modified for plotting since regression functions have changed)
 def plotPrediction(data, zipCode, predictions):
-    plt.scatter(data[data['zip_code']==zipCode].loc[:,'acre_lot'], data[data['zip_code']==zipCode].loc[:,'price'])
-    plt.scatter(data[data['zip_code']==zipCode].loc[:,'acre_lot'], predictions)
+    x, yActual, yPredicted = dask.compute(data[data['zip_code']==zipCode].loc[:,'acre_lot'], data[data['zip_code']==zipCode].loc[:,'price'], predictions)
+    plt.scatter(x, yActual)
+    plt.scatter(x, yPredicted)
     plt.show()
     pass
 
-# Run all of the fuctions together as one unit
-def runRegressor(data, zipCode):
-    groupedData = zipGroupArray(data)
-    predictions = prediction(data, groupedData, zipCode)
-    plotPrediction(data, zipCode, predictions)
+# Run the regression
+def runRegressor(data):
+    return dask.compute(zipGroupArray(data))
+
+# Exports the data to csv
+def exportData(data, file = 'datasets/predicted-data.csv'):
+    data.to_csv(file)
     pass
 
- 
+# Filters data based on criteria in a dictionary
+def filterData(data, criteria):
+    return data[data.eval(criteria)]
+
+# Test the entire program 
 if __name__ == '__main__':
     # Set file where data can be found
-    file = './datasets/realtor-data.csv'
+    file = 'datasets/realtor-data.csv'
     
     # Set up a Dask Client
     client = Client(n_workers=4)
 
     # Set dataframe variable
     datadf = readData(file)
-    
-    # A function to test plotting a sample set from the dataset
-    def plotSample(samples):
-        # Create a sample set of the data
-        sample = datadf.head(1000)
 
-        # Create a subset of the sample without NA values
-        subSample = sample[['bed', 'bath', 'acre_lot', 'house_size', 'zip_code', 'price']].dropna()
+    # Run regression to predict prices
+    runRegressor(datadf)
 
-        # Test the regressor on each zip code in the subset of the sample
-        for eachCode in subSample['zip_code'].unique()[0:samples]:
-            runRegressor(subSample, eachCode)
-            pass
-        pass
-
-    # A function to test plotting the whole dataset
-    def plotData(plots):
-        # Drop NA values for regression
-        subSample = datadf[['bed', 'bath', 'acre_lot', 'house_size', 'zip_code', 'price']].dropna()
-
-        # Test the regressor on each zip code in the subset of the sample
-        for eachCode in subSample['zip_code'].unique()[0:plots]:
-            runRegressor(subSample, eachCode)
-            pass
-        pass
-
-    # Plot one zip code from the sample set
-    plotSample(1)
-
-    # Plot one zip code from the full data set
-    plotData(1)
-
+    # Export dataframe to csv
+    exportData(datadf)
 
     # Close the client
     client.close()
-
-
