1364dd471bdd11aa8814b58d7d75e900553ed84e
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-06 16:53:08 +0000

    Testing filtering functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import createdb as db
 import filterData as filter
-import os
 
 st.title("Nick Carroll's Real Estate Analysis App")
 
@@ -10,12 +9,9 @@ beds = int(st.text_input("Please input the number of bed that you are looking fo
 baths = float(st.text_input("Please input the number of bathrooms that you are looking for in your home:", "2"))
 sqft = float(st.text_input("Please input the square footage that you are looking for in your home:", "1000"))
 
-st.write(os.getenv("AWS_REALESTATE_PORT"))
 df = db.getData()
-st.table(df.tail())
 criteria = f"(bed == {beds})"
-st.write(criteria)
-st.table(data[data.eval(criteria)].head())
+st.table(df[df.eval(criteria)].head())
 # filteredDF = filter.filterdf(df, criteria)
 st.subheader("The top homes that match your search criteria are:")
 # st.table(filteredDF.head())
